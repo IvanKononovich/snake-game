43ec8b8003da4dd8f28adbe55ae2f1aa5b4079c4
Ivan Kononovich <fogfug1@gmail.com>
2019-04-07 18:10:06 +0300

    feat: add class partBody

--- js/script.js
+++ js/script.js
@@ -6,23 +6,21 @@ const ctx = canv.getContext('2d');
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
 
+const listElements = [];
 
 class Head {
     constructor(options) {
         this.width = options.width;
         this.height = options.height;
         this.color = options.color;
-        this.x = options.x;
+        this.x = options.y;
         this.y = options.y;
         this.speed = options.speed;
         this.directionHorizontal = this.speed;
         this.directionVertical = 0;
-
-        this.draw = this.draw.bind(this);
-
-        this.control();
-
-        requestAnimationFrame(this.draw);
+        this.listCoordsWay = [];
+        
+        this.control()
     }
 
     control() {
@@ -31,14 +29,17 @@ class Head {
                 this.directionVertical = 0;
                 this.directionHorizontal = this.speed;
             }
+
             if(e.keyCode === 37) {
                 this.directionVertical = 0;
                 this.directionHorizontal = this.speed * -1;
             }
+
             if(e.keyCode === 38) {
                 this.directionHorizontal = 0;
                 this.directionVertical = this.speed * -1;
             }
+
             if(e.keyCode === 40) {
                 this.directionHorizontal = 0;
                 this.directionVertical = this.speed;
@@ -46,20 +47,14 @@ class Head {
         });
     }
 
-    draw() {
-        ctx.clearRect(0, 0, canv.width, canv.height);
-        ctx.beginPath();
+    move() {
+        this.listCoordsWay.push([this.x, this.y]);
+        if(this.listCoordsWay.length > this.width / this.speed + 1) this.listCoordsWay.shift();
 
-        ctx.rect(this.x + this.directionHorizontal, this.y + this.directionVertical, this.width, this.height);
-        
         this.x += this.directionHorizontal;
         this.y += this.directionVertical;
 
         this.paymentDirections();
-
-        ctx.fill();
-
-        requestAnimationFrame(this.draw);
     }
 
     paymentDirections() {
@@ -86,11 +81,68 @@ class Head {
 
 }
 
+class PartBody {
+    constructor(options) {
+        this.width = options.width;
+        this.height = options.height;
+        this.color = options.color;
+        this.guiding = options.guiding;
+        this.x = this.guiding.x;
+        this.y = this.guiding.y;
+        this.speed = options.speed;
+        this.listCoordsWay = [];
+    }
+
+    move() {
+        this.listCoordsWay.push([this.x, this.y]);
+        if(this.listCoordsWay.length > this.width / this.speed + 1) this.listCoordsWay.shift();
+        
+        this.x = this.guiding.listCoordsWay[0][0];
+        this.y = this.guiding.listCoordsWay[0][1];
+    }
+}
+
 const head = new Head({
-    width: 20,  
-    height: 20,
-    color: '#000',
-    x: 100,
-    y: 100,
+    width: 25,  
+    height: 25,
+    color: 'red',
+    x: 10,
+    y: 10,
     speed: 5
 });
+listElements.push(head);
+
+const partBody = new PartBody({
+    width: 25,  
+    height: 25,
+    color: '#000',
+    guiding: head,
+    speed: 5
+});
+listElements.push(partBody);
+
+requestAnimationFrame(function draw() {
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    listElements.forEach(item => {
+        ctx.beginPath();
+        ctx.fillStyle = item.color;
+        ctx.rect(item.x, item.y, item.width, item.height);
+        ctx.fill();
+        item.move();
+    });
+
+    requestAnimationFrame(draw);
+});
+
+
+for (let i = 0; i < 10; i++) {
+    const partBody = new PartBody({
+        width: 25,  
+        height: 25,
+        color: '#000',
+        guiding: listElements[listElements.length - 1],
+        speed: 5
+    });
+
+    listElements.push(partBody);
+}
